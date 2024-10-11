40e43a68c1d49aec0439e04b831b74e77def5279
Basher <basher@i3ash.com>
2024-10-11 17:45:12 +0800

    Update protobuf@25 rev 3

--- Formula/protobuf@25.rb
+++ Formula/protobuf@25.rb
@@ -1,86 +1,15 @@
 class ProtobufAT25 < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://protobuf.dev/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v25.5/protobuf-25.5.tar.gz"
-  sha256 "3cf7d5b17c4ff04fe9f038104e9d0cae6da09b8ce271c13e44f8ac69f51e4e0f"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v25.5/protoc-25.5-osx-universal_binary.zip"
+  sha256 "a8cd6ce419b9e150c560ca266eee8913b59052c328691a24ef047ee1da014f07"
   license "BSD-3-Clause"
-  revision 2
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
-
-  depends_on "cmake" => :build
-  depends_on "python-setuptools" => :build
-  depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.12" => [:build, :test]
-  depends_on "abseil"
-  depends_on "jsoncpp"
-
-  uses_from_macos "zlib"
-
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .map { |f| f.opt_libexec/"bin/python" }
-  end
-
+  revision 3
   def install
-    # Keep `CMAKE_CXX_STANDARD` in sync with the same variable in `abseil.rb`.
-    abseil_cxx_standard = 17
-    cmake_args = %w[
-      -DBUILD_SHARED_LIBS=ON
-      -Dprotobuf_BUILD_LIBPROTOC=ON
-      -Dprotobuf_BUILD_SHARED_LIBS=ON
-      -Dprotobuf_INSTALL_EXAMPLES=ON
-      -Dprotobuf_BUILD_TESTS=OFF
-      -Dprotobuf_ABSL_PROVIDER=package
-      -Dprotobuf_JSONCPP_PROVIDER=package
-    ]
-    cmake_args << "-DCMAKE_CXX_STANDARD=#{abseil_cxx_standard}"
-
-    system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
-
-    (share/"vim/vimfiles/syntax").install "editors/proto.vim"
-    elisp.install "editors/protobuf-mode.el"
-
-    ENV.append_to_cflags "-I#{include}"
-    ENV.append_to_cflags "-L#{lib}"
-    ENV["PROTOC"] = bin/"protoc"
-
-    cd "python" do
-      # Keep C++ standard in sync with `abseil.rb`.
-      inreplace "setup.py", "extra_compile_args.append('-std=c++14')",
-                            "extra_compile_args.append('-std=c++#{abseil_cxx_standard}')"
-
-      pythons.each do |python|
-        pyext_dir = prefix/Language::Python.site_packages(python)/"google/protobuf/pyext"
-        with_env(LDFLAGS: "-Wl,-rpath,#{rpath(source: pyext_dir)} #{ENV.ldflags}".strip) do
-          system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
-        end
-      end
-    end
+    bin.install "bin/protoc"
+    include.install Dir["include/*"]
   end
-
   test do
-    testdata = <<~EOS
-      syntax = "proto3";
-      package test;
-      message TestCase {
-        string name = 4;
-      }
-      message Test {
-        repeated TestCase case = 1;
-      }
-    EOS
-    (testpath/"test.proto").write testdata
-    system bin/"protoc", "test.proto", "--cpp_out=."
-
-    pythons.each do |python|
-      system python, "-c", "from google.protobuf.pyext import _message"
-    end
+    system bin/"protoc", "--version"
   end
 end
