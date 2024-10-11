97c5d2d38cfdb7fa4a94480470bbd110b077ad76
Basher <basher@i3ash.com>
2024-10-11 22:35:49 +0800

    Release fortify v1.0.13 revision 10

--- Formula/fortify.rb
+++ Formula/fortify.rb
@@ -1,18 +1,62 @@
 class Fortify < Formula
   desc "Command-line tool designed to enhance file security through encryption"
   homepage "https://github.com/i3ash/fortify"
-  url "https://github.com/i3ash/fortify/archive/refs/tags/v1.0.12.tar.gz"
-  sha256 "d323e84712d691eafdc5683364a567632c61c2b106a656c9120d71b39939ea4f"
+  version "1.0.13"
   license "MIT"
+  revision 10
+
+  option "with-universal", "Install the universal binary"
+
   depends_on "go" => :build
+
+  resource "source" do
+    url "https://github.com/i3ash/fortify/archive/refs/tags/v1.0.13.tar.gz"
+    sha256 "1f82358abb418d3600fe22a6d838e9c7c5b9ee158e1816e9d288e3450bb26983"
+  end
+
+  if build.with? "universal"
+    url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-universal"
+    sha256 "2fccdb13bf87d8d44e000f137ddce3eb2be72eef5f7966d60a923678be3a989b"
+  else
+    on_arm do
+      url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-arm64"
+      sha256 "361aa1bb6ef80ee831b9a014e8b1d24754c949407674f56e418468654b6237bf"
+    end
+    on_intel do
+      url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-x86_64"
+      sha256 "5e0f9df3daac05fcbcadc95813c8d9b14f814151bd68e97b80030c9b1551f286"
+    end
+  end
+
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/i3ash/fortify").install buildpath.children
-    cd "src/github.com/i3ash/fortify" do
+    binary_name = case
+    when build.with?("universal")
+      "fortify-darwin-universal"
+    when Hardware::CPU.arm?
+      "fortify-darwin-arm64"
+    when Hardware::CPU.intel?
+      "fortify-darwin-x86_64"
+    else
+      opoo "Unsupported architecture, building from source..."
+      return build_from_source
+    end
+    if File.exist?(binary_name)
+      chmod "+x", binary_name
+      bin.install binary_name => "fortify"
+    else
+      opoo "Binary file not found, building from source..."
+      build_from_source
+    end
+  end
+
+  def build_from_source
+    resource("source").stage do
       system "go", "build", *std_go_args(ldflags: "-s -w"), "."
     end
   end
+
   test do
-    system bin/"fortify", "version"
+    system bin/"fortify"
+    assert_match "v#{version}", shell_output("#{bin}/fortify version")
   end
 end
