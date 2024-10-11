661dc440beaac8b4f7b0f970eb405c92520b4e4e
Basher <basher@i3ash.com>
2024-10-11 17:35:58 +0800

    Update protobuf@25

--- Formula/protobuf@25.rb
+++ Formula/protobuf@25.rb
@@ -4,21 +4,11 @@ class ProtobufAT25 < Formula
   url "https://github.com/protocolbuffers/protobuf/releases/download/v25.5/protobuf-25.5.tar.gz"
   sha256 "3cf7d5b17c4ff04fe9f038104e9d0cae6da09b8ce271c13e44f8ac69f51e4e0f"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    sha256                               arm64_sonoma:   "fddad1ce30679b586263d42f6873ad14033bf081f1a02265a2dfd0db6c5b90a9"
-    sha256                               arm64_ventura:  "e5f9660c2add8a0b25a739d8733f8e39994627a52c4280fda3f6a6952fd3580c"
-    sha256                               arm64_monterey: "25697e79cbf8a2aa5ae3c70e2a03700cd8c071a1731ecc3eb89bcf44e7d70310"
-    sha256                               sonoma:         "f39583032815a31f5d31b520e24d70f75755e921235496b29fd1ed964fd6bcb5"
-    sha256                               ventura:        "7a1992bf50282507f8a5b707b98f85d02cc95da61b4a77145ecaa7dc48905f5f"
-    sha256                               monterey:       "80f58bba003e5b796d80d00a9324326431340a44abcbcf722596db9c95fc7055"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56699682719d48c950eb9e0e862669759e974d9120b08b3d004a61fdd4b4bae8"
   end
 
   depends_on "cmake" => :build
