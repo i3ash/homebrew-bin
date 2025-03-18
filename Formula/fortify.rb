7d740eb112d401eeb754f04ffcdf5b90167224d9
Basher <basher@i3ash.com>
2025-03-18 11:03:07 +0800

    Update Fortify formula to version 1.2.3
    - Adjust version and SHA256 checksums for the new release
    - Ensure correct URLs for both universal and architecture-specific binaries

--- Formula/fortify.rb
+++ Formula/fortify.rb
@@ -1,7 +1,7 @@
 class Fortify < Formula
   desc "Command-line tool designed to enhance file security through encryption"
   homepage "https://github.com/i3ash/fortify"
-  version "1.1.6"
+  version "1.2.3"
   license "MIT"
 
   option "with-universal", "Install the universal binary"
@@ -9,21 +9,21 @@ class Fortify < Formula
   depends_on "go" => :build
 
   resource "source" do
-    url "https://github.com/i3ash/fortify/archive/refs/tags/v1.1.6.tar.gz"
-    sha256 "bca0d701d7381c2dbe63a788ed20ad5efed97325379803b36e3d2509496c67a0"
+    url "https://github.com/i3ash/fortify/archive/refs/tags/v1.2.3.tar.gz"
+    sha256 "c642cf7f0321c281b9f5a96ee013cd13ec1b4faca56f84cc257410f32782cc78"
   end
 
   if build.with? "universal"
     url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-universal"
-    sha256 "9b2d7efd05e9465ab5b75bf21edebce9b13caf86f6bd2bc12dc383d629b2728b"
+    sha256 "4760718119ea746bf52403a0e3d602d4c696f2321db19736faefe1fccb88cf45"
   else
     on_arm do
       url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-arm64"
-      sha256 "ac91d0b6a33c18ddc4a417bd8f76f42b16de24c34633457011acf01861927023"
+      sha256 "4a07783fe7fd1b4dc8aeab36acae02f111e6a8f998150fcb5bb4502187d50a93"
     end
     on_intel do
       url "https://github.com/i3ash/fortify/releases/download/v#{version}/fortify-darwin-x86_64"
-      sha256 "96beaac1baa9ec08ce26e7397120aa9d11dc3d0bf19b4fbf46eed30f1a7245c3"
+      sha256 "811761ccada16fb2225a74ffd8b70706fe00f79cbfac95a68436c262c0d5adc5"
     end
   end
 
